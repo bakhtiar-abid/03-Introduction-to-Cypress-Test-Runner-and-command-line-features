e17f58bcea12563bd95139cb738a241748ab70a4
Bakhtiar Abid <70568315+bakhtiarabid@users.noreply.github.com>
2022-12-21 01:31:40 +0600

    First Test Case

---
+++ cypress/e2e/3-first-test/Test1.cy.js
@@ -0,0 +1,12 @@
+describe("My First Test Suite", () => {
+   it("My FirstTest case", () => {
+      //test step
+      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+   });
+
+   // it('My SecondTest case', ()=>{
+
+   // //test step
+
+   // })
+});
