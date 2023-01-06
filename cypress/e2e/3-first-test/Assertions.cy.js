834f7682769a90e3e6a3751bea9180f3e5233204
Bakhtiar Abid <70568315+bakhtiarabid@users.noreply.github.com>
2023-01-06 20:38:54 +0600

    Explicit and Implicit assertions

--- cypress/e2e/3-first-test/Assertions.cy.js
+++ cypress/e2e/3-first-test/Assertions.cy.js
@@ -2,6 +2,7 @@
 
 
 
+
 describe("Assertions Demo", ()=>{
     it('Implicit assertions', () => {
         
@@ -10,7 +11,7 @@ describe("Assertions Demo", ()=>{
         );
 
 
-        //should and ==> assertions
+        //should and ==> implicit assertions, cypress built assertions
 
         // cy.url().should("include", "orangehrmlive.com");
         // cy.url().should(
@@ -36,9 +37,55 @@ describe("Assertions Demo", ()=>{
               "eq",
               "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
            )
-           .and("contain", "orangehrm");
+           .and("contain", "orangehrm")
+           .and("not.contain", "greenhrm")
+
+           cy.title().should('include', 'Orange')
+           .and('eq', 'OrangeHRM')
+           .and('contain', "HRM")
+
+           cy.get(".orangehrm-login-branding > img").should("be.visible")
+           .and("exist") // validating wether the company logo is visible or not
 
 
+           cy.xpath("//a").should('have.length', '5') 
+           // validating no of links are present in the webpage
+
+           cy.get("input[placeholder='Username']").type("Admin") 
+           // provide a value into inputbox
+
+           cy.get("input[placeholder='Username']").should('have.value', "Admin")
+            // input value checking
+
+           
+
+    });
+
+
+    it('Explicit assertions', () => {
+        
+        cy.visit(
+           "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        );
+      cy.get("input[placeholder='Username']").type("Admin");
+      cy.get("input[placeholder='Password']").type("admin123");
+      cy.get("button[type='submit']").click()
+
+
+      let expName = "xyz";
+
+      cy.get(".oxd-userdropdown-name").then( (x)=>{
+         let actName = x.text();
+
+         // Explicit Assertion in BDD Approach
+         expect(actName).to.equal(expName);
+         expect(actName).to.not.equal(expName);
+
+         //TDD Style
+         assert.equal(actName, expName)
+         assert.notEqual(actName, expName)
+
+      })
 
     });
 })
